3b63f6e6460809465120b654a0fdd2aea56666fb
abescos <abescos@hiberus.com>
2023-01-30 15:22:00 +0100

    Add React router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './assets/styles/components/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <p>En Terapia</p>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' exact />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
